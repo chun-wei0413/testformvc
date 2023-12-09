f2b30307c3b038875d5a2c1cfc9859ad3a3da6bd
409262671 <90028097+chun-wei0413@users.noreply.github.com>
2023-12-10 00:04:06 +0800

    Add files via upload

---
+++ testfotmvc/src/main/java/com/example/testfotmvc/TestfotmvcApplication.java
@@ -0,0 +1,13 @@
+package com.example.testfotmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestfotmvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestfotmvcApplication.class, args);
+	}
+
+}
